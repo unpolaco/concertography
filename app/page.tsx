07cedbb58b354e9b86097ea92149aaf158141626
Michal Borowski <michal.borowski@hltech.com>
2023-07-15 09:38:27 +0200

    Get data from Cloudinary folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import getResults from '@/utils/getImagesData'
 
-export default function Home() {
+
+export default async function Home () {
+const {imagesData, bandNames, genries, eventPlaces, cities} = await getResults()
+
   return (
-    <main className={styles.main}>
-      <div>
-
-
-      </div>
-    </main>
+    <>
+      <main className={styles.main}>
+        <div>
+          <form id="searchForm">
+            <label htmlFor="search">Looking for something?</label>
+            <input type='text' id="search" name="search" />
+            <button type='submit' form='searchForm'>Search</button>
+          </form>
+          <div className={styles.imageWrapper}>
+          </div>
+            <p>{...bandNames}</p>
+        </div>
+      </main>
+    </>
   )
 }
